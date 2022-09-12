067a3e9efb65c062948f844ecd92a40c349ac491
Rohini Bali <64684079+Rohini-Bali@users.noreply.github.com>
2022-09-12 16:21:55 +0530

    Use npx express-generator to build skeleton of server app

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
-const express = require('express')
-const helmet = require('helmet') //Helmet is Express middleware. (It also works with Connect or no library at all! If you need support for other frameworks or languages, see this list.) The top - level helmet function is a wrapper around 15 smaller middlewares.
-const cors = require('cors')
-const app = express()
-const port = 3000
-var whitelist = ['http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+import express from "express";
+import helmet from "helmet";
+// eslint-disable-next-line max-len
+// Helmet is Express middleware.(It also works with Connect or no library at all! If you need support for other frameworks or languages, see this list.) The top - level helmet function is a wrapper around 15 smaller middlewares.
+import cors from "cors";
+const app = express();
+const port = 3000;
 
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.get("/", (req, res)=>{
+  res.send("Hello World! :)");
+});
 app.use(helmet());
-app.get('/', cors(corsOptions), (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", cors(corsOptions), (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  console.log(`CORS-enabled web server listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+  console.log(`CORS-enabled web server listening on port ${port}`);
+});
